ecf57eab8fbaba985b8a71c1bbbe9d0edce45381
andressaray14@gmail.com <andressaray14@gmail.com>
2022-12-12 11:15:03 -0500

    fix: Skeleton mobile

--- src/components/SkeletonCharacter/index.tsx
+++ src/components/SkeletonCharacter/index.tsx
@@ -3,13 +3,13 @@ const SkeletonCharacter = () => {
     <div>
       <div
         role="status"
-        className="w-1/2 mx-auto animate-pulse dark:border-gray-700"
+        className="sm:w-1/2 mx-auto animate-pulse dark:border-gray-700"
       >
-        <div className="flex items-center justify-center shadow-lg rounded-lg w-full p-4 bg-gray-500">
+        <div className="flex flex-col sm:flex-row items-center justify-center shadow-lg rounded-lg w-full p-4 bg-gray-500">
           <div>
             <div className="h-60 w-60 bg-gray-600 rounded-full"></div>
           </div>
-          <div className="ml-4 w-1/2">
+          <div className="ml-4 sm:w-1/2 mt-4">
             <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
             <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
             <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
@@ -20,7 +20,7 @@ const SkeletonCharacter = () => {
       </div>
       <div
         role="status"
-        className="w-1/2 h-60 mx-auto p-8 mt-5 rounded-lg animate-pulse dark:border-gray-700 bg-gray-500"
+        className="w-full sm:w-1/2 h-60 mx-auto p-8 mt-5 rounded-lg animate-pulse dark:border-gray-700 bg-gray-500"
       >
         <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
         <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
