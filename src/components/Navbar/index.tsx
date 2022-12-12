f18d920ef4eb4cc9a70a19b6b8e1db8ab60c8f18
andressaray14@gmail.com <andressaray14@gmail.com>
2022-12-12 11:44:42 -0500

    fix: Navbar open to mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const { pathname } = useRouter();
 
   const routes = [
@@ -18,31 +20,55 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="sm:border-gray-200 px-2 sm:px-4 py-.5 rounded ">
-      <div className="container flex flex-wrap items-center justify-end mx-auto">
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <div className="flex items-center justify-end mr-4">
+      <nav>
+        <section className="flex my-4 md:hidden">
+          <div
+            className="space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
           >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <span className="block h-0.5 w-8 bg-white"></span>
+            <span className="block h-0.5 w-8 bg-white"></span>
+            <span className="block h-0.5 w-8 bg-white"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-white"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <div className="flex flex-col items-center justify-between min-h-[250px]">
+              {routes.map((route) => (
+                <Link href={route.route} key={route.id}>
+                  <p
+                    className={`font-bold underline ${
+                      route.route === pathname ? "text-[#87CD53]" : "text-white"
+                    }`}
+                  >
+                    {route.name}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </section>
+        <div
+          className="hidden lg:flex w-full md:block md:w-auto"
+          id="navbar-default"
+        >
           <div className="flex flex-col p-4 mt-4 justify-end border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
             {routes.map((route) => (
               <Link
@@ -57,8 +83,27 @@ const Navbar = () => {
             ))}
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <style>{`
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: #000;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+    `}</style>
+    </div>
   );
 };
 
